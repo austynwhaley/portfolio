d92af5c8bba4d169f325ab4589145a67157ca89f
Austyn Whaley <austyn_whaley@yahoo.com>
2021-12-08 12:31:48 -0500

    home button fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <nav>
             <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="/portfolio">Home</a></li>
             <li><a href="/portfolio/#/about/">About Me</a></li>
             <li><a href="/portfolio/#/portfolio/">Portfolio</a></li>
             </ul>
