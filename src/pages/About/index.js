c7bae9387617d5364ed7afe766a5995178553e36
Austyn Whaley <austyn_whaley@yahoo.com>
2021-12-15 14:35:48 -0500

    spell check

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -37,7 +37,7 @@ function About () {
                     <p style={{fontSize: '1.4rem'}}>My Name is Austyn Whaley, I am 27 Years old and I was born in Cincinnati Ohio.</p> 
                     <p style={{fontSize: '1.4rem'}}>I grew up all around different areas in Northern Kentucky and went to a few different schools before I ended up in Belleuve,KY; and there is where I graduated from Bellevue High School in 2012.</p>
                     <p style={{fontSize: '1.4rem'}}>I currently I work at 'LiveShopper Sassie' on the IT team as a Support Developer.</p>
-                    <p style={{fontSize: '1.4rem'}}> I've also recently learned blockchain and Web3 developemnt learing smart contracts on Etheruem</p>
+                    <p style={{fontSize: '1.4rem'}}> I've also recently learned blockchain and Web3 development learning smart contracts on Ethereum</p>
                     <p style={{fontSize: '1.4rem'}}> I'm hard working and a dedicated developer constantly looking for new projects, new ideas and opportunities</p>
                     <p style={{fontSize: '1.4rem'}}> Thanks for reading and interest</p>
                 </div>
